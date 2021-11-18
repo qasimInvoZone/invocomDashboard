39219a893f7b745001e30a213a1d4525ba0899c0
qasimInvoZone <qasim.rehman@invozone.com>
2021-11-18 12:59:56 +0500

    final commit

--- src/router/Router.js
+++ src/router/Router.js
@@ -89,7 +89,7 @@ const Router = () => {
       return <Redirect to='/login' />
     } else if (route.meta && route.meta.authRoute && isUserLoggedIn()) {
       // ** If route has meta and authRole and user is Logged in then redirect user to home page (DefaultRoute)
-      return <Redirect to='/' />
+      return <Redirect to='/home' />
     } else if (isUserLoggedIn() && !ability.can(action || 'read', resource)) {
       // ** If user is Logged in and doesn't have ability to visit the page redirect the user to Not Authorized
       return <Redirect to='/misc/not-authorized' />
@@ -147,7 +147,6 @@ const Router = () => {
                         ...props,
                         meta: route.meta
                       })
-                      return isLoggedIn() ? <Redirect to={route.path}/> : <Redirect to={DefaultRoute}/> 
                     }}
 
                     render={props => {
@@ -219,7 +218,7 @@ const Router = () => {
           exact
           path='/not-authorized'
           render={()=> {
-            return isLoggedIn() ? <Redirect to='/home'/> : <Layouts.BlankLayout> <NotAuthorized /> </Layouts.BlankLayout>
+            return isUserLoggedIn() ? <Redirect to='/home'/> : <Layouts.BlankLayout> <NotAuthorized /> </Layouts.BlankLayout>
           }}
         />
         {ResolveRoutes()}
