8827d5d0a50b77927a8593b3fb9ba3f7bb930f67
Adeel <adeel.yaseen@invozone.com>
2021-12-02 17:52:02 +0500

    Setting Access for Admin and Super Admin

--- src/views/apps/chat/ChatModal/index.js
+++ src/views/apps/chat/ChatModal/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {
   Button,
   Modal,
@@ -13,6 +13,7 @@ const ModalForm = (props) => {
   const [formModal, setFormModal] = useState(true);
   const [assignee, setAssignee] = useState("");
   const [isAssignee, setIsAssignee] = useState(false);
+  const [user, setUser] = useState({});
   const assignChat = async () => {
     const baseUrl = process.env.REACT_APP_INVOCOM_API_URL;
     const apiVersion = process.env.REACT_APP_INVOCOM_API_VERSION;
@@ -47,40 +48,48 @@ const ModalForm = (props) => {
       return <option value={admin._id}>{admin.email}</option>;
     });
   };
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    setUser(user);
+  }, [setUser]);
   return (
     <div className="demo-inline-spacing">
-      <Modal
-        isOpen={formModal}
-        toggle={() => setFormModal(!formModal)}
-        className="modal-dialog-centered"
-      >
-        <ModalHeader toggle={() => setFormModal(!formModal)}></ModalHeader>
-        <ModalBody>
-          <h2>Assign the conversation</h2>
-          <FormGroup>
-            <Label for="email">Email:</Label>
-            <div className="notification-dropdown-chatHistory">
-              <select onChange={(event) => setAssignee(event.target.value)}>
-                <option value="">Select Admin</option>
-                {renderAdmins(props.admins)}
-              </select>
-            </div>
-          </FormGroup>
-        </ModalBody>
-        <ModalFooter>
-          <Button color="info" onClick={() => assignChat()}>
-            Assign
-          </Button>
+      {user?.role === "ADMIN" || user?.role === "SUPER_ADMIN" ? (
+        <Modal
+          isOpen={formModal}
+          toggle={() => setFormModal(!formModal)}
+          className="modal-dialog-centered"
+        >
+          <ModalHeader toggle={() => setFormModal(!formModal)}></ModalHeader>
+          <ModalBody>
+            <h2>Assign the conversation</h2>
+            <FormGroup>
+              <Label for="email">Email:</Label>
+              <div className="notification-dropdown-chatHistory">
+                <select onChange={(event) => setAssignee(event.target.value)}>
+                  <option value="">Select Admin</option>
+                  {renderAdmins(props.admins)}
+                </select>
+              </div>
+            </FormGroup>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="info" onClick={() => assignChat()}>
+              Assign
+            </Button>
 
-          <Button
-            className="close_button"
-            color="primary"
-            onClick={() => setFormModal(!formModal)}
-          >
-            Close
-          </Button>
-        </ModalFooter>
-      </Modal>
+            <Button
+              className="close_button"
+              color="primary"
+              onClick={() => setFormModal(!formModal)}
+            >
+              Close
+            </Button>
+          </ModalFooter>
+        </Modal>
+      ) : (
+        ""
+      )}
       {/* {renderAdmins(props.admins)} */}
     </div>
   );
--- src/views/apps/chat/SidebarLeft.js
+++ src/views/apps/chat/SidebarLeft.js
@@ -1,60 +1,80 @@
 // ** React Imports
-import User1 from '../../../assets/images/avatars/demo_user.png'
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import { Search } from 'react-feather'
-import { useHistory } from 'react-router-dom'
+import User1 from "../../../assets/images/avatars/demo_user.png";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Search } from "react-feather";
+import { useHistory } from "react-router-dom";
 const SidebarLeft = ({ renderChatParent, chats }) => {
-  const history = useHistory()
-  const token = localStorage.getItem('token');
-  if(!token){
-    history.push('/login');
+  const history = useHistory();
+  const token = localStorage.getItem("token");
+  if (!token) {
+    history.push("/login");
   }
-  const [user, setUser] = useState({})
-  const [search, setSearch] = useState('')
+  const [user, setUser] = useState({});
+  const [search, setSearch] = useState("");
   useEffect(async () => {
-    const user = JSON.parse(localStorage.getItem('user'))
-    const token = localStorage.getItem('token')
+    const user = JSON.parse(localStorage.getItem("user"));
+    const token = localStorage.getItem("token");
 
-    setUser(user)
+    setUser(user);
+  }, [axios, setUser]);
 
-  }, [axios, setUser])
-
- function renderChat(chat_id) {
-   renderChatParent(chat_id)
- }
-  return  (
-
+  function renderChat(chat_id) {
+    renderChatParent(chat_id);
+  }
+  return (
     <>
-    <div className="left-section mCustomScrollbar" data-mcs-theme="minimal-dark">
-    <div className="headLeft-section">
-      <div className="headLeft-sub">
-        <Search className="search_icon_chat" />
-        <input type="text" name="search" placeholder="Search..." onChange={(e) => setSearch(e.target.value)}/>
+      <div
+        className="left-section mCustomScrollbar"
+        data-mcs-theme="minimal-dark"
+      >
+        <div className="headLeft-section">
+          <div className="headLeft-sub">
+            <Search className="search_icon_chat" />
+            <input
+              type="text"
+              name="search"
+              placeholder="Search..."
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </div>
+        </div>
+        {user?.role === "SUPER_ADMIN" || user?.role === "ADMIN" ? (
+          <ul>
+            {chats?.length > 0 &&
+              chats?.map((chat) => (
+                <li
+                  href="/apps/chat/messages"
+                  className="active"
+                  onClick={() => renderChat(chat._id)}
+                >
+                  <div className="chatList">
+                    <div className="img">
+                      <img src={User1} />
+                    </div>
+                    <div className="desc">
+                      {chat.unReadMessages > 0 && (
+                        <div className="counter">{chat.unReadMessages}</div>
+                      )}
+                      <h5 style={{ fontWeight: "bold" }}>
+                        {user?.role === "ADMIN" || user?.role === "SUPER_ADMIN"
+                          ? chat?.client?.fullname
+                          : chat?.superAdmin?.fullname}
+                      </h5>
+                      <p>
+                        {chat?.messages[chat?.messages?.length - 1]?.message}
+                      </p>
+                    </div>
+                  </div>
+                </li>
+              ))}
+          </ul>
+        ) : (
+          "There is no chat assigned to you"
+        )}
       </div>
-    </div>
-        <ul>
-          {
-            chats?.length > 0 && chats?.map(chat => (
-              <li href="/apps/chat/messages" className="active" onClick={() => renderChat(chat._id)}>
-              <div className="chatList">
-                <div className="img">
-                  <img src={User1} />
-                </div>
-                <div className="desc">
-              {
-                chat.unReadMessages > 0 &&<div className="counter">{chat.unReadMessages}</div>
-              }
-              <h5 style={{fontWeight:"bold"}}>{user?.role === 'ADMIN' || user?.role === 'SUPER_ADMIN' ?  chat?.client?.fullname : chat?.superAdmin?.fullname}</h5>
-              <p>{ chat?.messages[chat?.messages?.length - 1]?.message }</p>
-                </div>
-              </div>
-            </li>
-            ))
-          }
-        </ul>
-      </div></>
-  )
-}
+    </>
+  );
+};
 
-export default SidebarLeft
+export default SidebarLeft;
